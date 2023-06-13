b2c4332499282221e4c2b25967e8888a3b2f2ceb
Thiago ERS <thiago.ers@gmail.com>
2023-06-12 23:09:32 -0300

    Changed typo in class name and  2 returns

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from cpf_validation import ValidadeCpfNumber
+from cpf_validation import ValidateCpfNumber
 
 
 def main():
 
-    cpf01=ValidadeCpfNumber()
+    cpf01=ValidateCpfNumber()
     cpf01.validate('113.472.377-63')
 
-    cpf02=ValidadeCpfNumber()
+    cpf02=ValidateCpfNumber()
     cpf02.validate('111.111.111-11')
 
-    cpf03=ValidadeCpfNumber()
+    cpf03=ValidateCpfNumber()
     cpf03.validate('11347237763')
 
 
